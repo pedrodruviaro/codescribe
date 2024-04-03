bd1c7325258adf0e33c360c0584e7432e007acb7
pedrodruviaro <pedrodruviaro@gmail.com>
2024-04-03 16:07:16 -0300

    feat: nuxt google fonts and external css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxtjs/google-fonts"],
 
   colorMode: {
     preference: "dark",
     fallback: "dark",
     storageKey: "codescribe-color-mode",
   },
+
+  googleFonts: {
+    families: {
+      "Fira Sans": [400, 600, 700],
+    },
+  },
+
+  css: ["~/assets/css/main.css"],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Home</h1>
+  <h1>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum reprehenderit
+    rem voluptates magni minus ut quae commodi temporibus, aliquam quisquam
+    tempora! Totam exercitationem rerum ea quam, debitis quae voluptatem ipsum.
+  </h1>
 </template>
